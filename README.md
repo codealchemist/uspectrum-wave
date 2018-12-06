bce63b60bf7dad5e4fac3e79355647b7c7dc82f8
Alberto Miranda <amiranda@elementum.com>
2018-12-05 22:56:30 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,60 @@
+# µ-spectrum-wave
+
+Wave spectrum analyzer for WebAudio.
+
+## Install
+
+`npm i uspectrum-wave`
+
+## Usage
+
+```
+import USpectrumWave from 'uspectrum-wave'
+
+const spectrum = new USpectrumWave({ context, buffer, source, canvas })
+spectrum
+  .init({ context: AudioContext, buffer: AudioBuffer, source: AudioSource })
+  .render()
+```
+
+## Constructor
+
+All params are optional.
+
+- `context`: An [AudioContext](https://developer.mozilla.org/en-US/docs/Web/API/AudioContext) object.
+- `buffer`: An [AudioBuffer](https://developer.mozilla.org/en-US/docs/Web/API/AudioBuffer) object.
+- `source`: An [AudioBufferSourceSource](https://developer.mozilla.org/en-US/docs/Web/API/AudioBufferSourceNode) object.
+- `canvas`: Can be:
+  - A CSS selector pointing to a canvas element rendered on the page.
+  - A reference to a canvas element.
+  - Unset: a canvas element will be created an appended to body.
+
+## Methods
+
+- `init({ context, buffer, source })`: Initializes audio analyzer and connects it to audio source. See **Constructor** for signature reference. Allows chaining.
+- `render`: Requests animation frame and starts rendering audio data with buffer updates.
+- `setFftSize(size)`: Sets [fftSize](https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/fftSize) property on AnalyzerNode. Default is 2048.
+- `setMinDb(db)`: Sets [minDecibels](https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/minDecibels) property on AnalyzerNode. Default is -90.
+- `setMaxDb(db)`: Sets [maxDecibels](https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/maxDecibels) property on AnalyzerNode. Default is -10.
+- `setSmoothing(smoothing)`: Sets [smoothingTimeConstant](https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/smoothingTimeConstant) on AnalyzerNode. Default is 0.85.
+- `setBackground(color)`: Sets canvas background color. Default is 'black'.
+- `setColor(color)`: Sets canvas line color. Default is 'yellow'.
+- `setLineWidth(width)`: Sets canvas line width. Default is 2.
+
+## Usage with µ-player
+
+**µ-player** is a WebAudio based programatic player that plays well with **µ-spectrum-wave**.
+
+```
+import Player from 'uplayer'
+
+const player = new Player('https://some.mp3')
+player
+  .on('play', () => {
+    spectrum
+      .init({ context: player.context, buffer: player.buffer, source: player.source })
+      .render()
+  })
+  .load()
+  .play()
+```
---
+++ index.js
@@ -0,0 +1,100 @@
+module.exports = class UspectrumWave {
+  constructor ({ context, buffer, source, canvas } = {}) {
+    if (typeof canvas === 'string') canvas = document.getElementById(canvas)
+    if (!canvas) {
+      canvas = document.createElement('canvas')
+      document.body.appendChild(canvas)
+    }
+    this.$canvas = canvas
+    this.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+    this.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    this.$canvas.width = this.width
+    this.$canvas.height = this.height
+    this.canvasContext = this.$canvas.getContext('2d')
+    this.background = 'black'
+    this.color = 'yellow'
+    this.lineWidth = 2
+    this.fftSize = 2048
+    this.minDb = -90
+    this.maxDb = -10
+    this.smoothing = 0.85
+
+    if (context && buffer && source) this.init({ context, buffer, source })
+  }
+
+  init ({ context, buffer, source }) {
+    this.analyser = context.createAnalyser()
+    this.analyser.fftSize = this.fftSize
+    this.analyser.minDecibels = this.minDb
+    this.analyser.maxDecibels = this.maxDb
+    this.analyser.smoothingTimeConstant = this.smoothing
+    this.analyser.buffer = buffer
+    source.connect(this.analyser)
+    return this
+  }
+
+  setFftSize (size) {
+    this.fftSize = size
+    return this
+  }
+
+  setMinDb (db) {
+    this.minDb = db
+    return this
+  }
+
+  setMaxDb (db) {
+    this.maxDb = db
+    return this
+  }
+
+  setSmoothing (smoothing) {
+    this.smoothing = smoothing
+  }
+
+  setBackground (color) {
+    this.background = color
+    return this
+  }
+
+  setColor (color) {
+    this.color = color
+    return this
+  }
+
+  setLineWidth (width) {
+    this.lineWidth = width
+    return this
+  }
+
+  render () {
+    const bufferLength = this.analyser.fftSize
+    const buffer = new Uint8Array(bufferLength)
+    window.requestAnimationFrame(() => this.render())
+    this.analyser.getByteTimeDomainData(buffer)
+    this.canvasContext.fillStyle = this.background
+    this.canvasContext.fillRect(0, 0, this.width, this.height)
+    this.canvasContext.lineWidth = this.lineWidth
+    this.canvasContext.strokeStyle = this.color
+    this.canvasContext.beginPath()
+
+    const sliceWidth = (this.width * 1.0) / bufferLength
+    let x = 0
+
+    for (let i = 0; i < bufferLength; i++) {
+      const v = buffer[i] / 128.0
+      const y = (v * this.height) / 2
+
+      if (i === 0) {
+        this.canvasContext.moveTo(x, y)
+      } else {
+        this.canvasContext.lineTo(x, y)
+      }
+
+      x += sliceWidth
+    }
+
+    this.canvasContext.lineTo(this.$canvas.width, this.$canvas.height / 2)
+    this.canvasContext.stroke()
+  }
+}
